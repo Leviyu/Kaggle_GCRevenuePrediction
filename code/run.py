d9a86d42a3682e3dcff77b4cf1ca0078db83ac1f
Ubuntu <ubuntu@ip-172-31-12-130.us-east-2.compute.internal>
2018-10-26 23:40:22 +0000

    add1

--- code/run.py
+++ code/run.py
@@ -9,10 +9,14 @@
 import os
 import sys
 import model_train
+import pandas as pd
 from model_train import lets_train
 
 sys.path.append('.')
 
+
+train_df = pd.read_hdf("../data/train_df.h5",key="train_df")
+test_df = pd.read_hdf("../data/test_df.h5",key="test_df")
 
 my_train = lets_train(train_df,test_df,'totals.transactionRevenue')
 my_train.run()
