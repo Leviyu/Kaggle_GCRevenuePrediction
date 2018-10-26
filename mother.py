d9d362f1abb264ff6faf9e9357009376af99a1cd
Leviyu <hongyu.hot@gmail.com>
2018-10-26 16:01:11 -0700

    clean

---
+++ mother.py
@@ -0,0 +1,38 @@
+"""
+__file__
+
+	mother.py
+
+__description__
+
+	This file is the main funciton for this exercise
+
+__author__
+
+	Hongyu Lai < hongyu.hot@gmail.com >
+
+"""
+
+import sys
+import numpy as numpy
+import pandas as pd
+
+################## 
+
+
+################## Read in train and test data
+
+
+################## Feature Engineering
+
+
+################## Define Models
+
+
+################## Train and Make predictions for each Model
+
+
+################## Ensemble
+
+
+
