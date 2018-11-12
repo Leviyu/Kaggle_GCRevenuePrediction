28b06efc36dbc558837e06108ed27c0648b9888d
Leviyu <hongyu.hot@gmail.com>
2018-11-12 15:42:04 -0700

    more

--- env_setup/env_setup.sh
+++ env_setup/env_setup.sh
@@ -1,9 +1,9 @@
 
 
+sudo apt install tcsh
 sudo apt-get install htop
-
 conda upgrade pandas
-
 conda install -c conda-forge lightgbm
-
 conda install -c conda-forge xgboost
+sudo -H pip install kaggle-cli
+
